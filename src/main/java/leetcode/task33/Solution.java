5cc7fea19a66871d4f6358facd408d6a26a56b52
Maksim Sofronov <Maksim_Sofronov@epam.com>
2022-10-14 12:52:26 +0600

    fixed task33

--- src/main/java/leetcode/task33/Solution.java
+++ src/main/java/leetcode/task33/Solution.java
@@ -8,6 +8,8 @@ class Solution {
 
     private int binarySearch(int[] nums, int left, int right, int target) {
         int midIndex = (left + right) / 2;
+        int decrementedMidIndex = Math.max(midIndex - 1, left);
+        int incrementedMidIndex = Math.min(midIndex + 1, right);
         int midValue = nums[midIndex];
         if (midValue == target) {
             return midIndex;
@@ -15,40 +17,40 @@ class Solution {
             return -1;
         }
 
-        boolean shiftOnTheRight = doesContainShift(nums, midIndex + 1, right);
-        boolean shiftOnTheLeft = doesContainShift(nums, left, midIndex - 1);
+        boolean shiftOnTheRight = doesContainShift(nums, incrementedMidIndex, right);
+        boolean shiftOnTheLeft = doesContainShift(nums, left, decrementedMidIndex);
 
-        boolean doesValueOnTheLeftIfLeftIsNotShifted = doesValueInsideNotShiftedArray(nums, left, midIndex - 1, target);
-        boolean doesValueOnTheRightIfRightIsNotShifted = doesValueInsideNotShiftedArray(nums, midIndex + 1, right, target);
+        boolean doesValueOnTheLeftIfLeftIsNotShifted = doesValueInsideNotShiftedArray(nums, left, decrementedMidIndex, target);
+        boolean doesValueOnTheRightIfRightIsNotShifted = doesValueInsideNotShiftedArray(nums, incrementedMidIndex, right, target);
 
         if (midValue < target) {
             // 1. if: shift on the RIGHT
             if (shiftOnTheRight) {
                 // 1.1. if: target on the LEFT
                 if (doesValueOnTheLeftIfLeftIsNotShifted) {
-                    return binarySearch(nums, left, midIndex - 1, target);
+                    return binarySearch(nums, left, decrementedMidIndex, target);
                 } else { // 1.2. else: target on the RIGHT
-                    return binarySearch(nums, midIndex + 1, right, target);
+                    return binarySearch(nums, incrementedMidIndex, right, target);
                 }
             }
             // 2. else if: shift on the LEFT
             else if (shiftOnTheLeft) {
                 // 2.1. if: target on the RIGHT
                 if (doesValueOnTheRightIfRightIsNotShifted) {
-                    return binarySearch(nums, midIndex + 1, right, target);
+                    return binarySearch(nums, incrementedMidIndex, right, target);
                 } else { // 2.2. else: target on the LEFT
-                    return binarySearch(nums, left, midIndex - 1, target);
+                    return binarySearch(nums, left, decrementedMidIndex, target);
                 }
             }
             // 3. else: no shift in new arrays
             else {
                 // 3.1. if: value on the LEFT
                 if (doesValueOnTheLeftIfLeftIsNotShifted) {
-                    return binarySearch(nums, left, midIndex - 1, target);
+                    return binarySearch(nums, left, decrementedMidIndex, target);
                 }
                 // 3.2. else: value on the RIGHT
                 else {
-                    return binarySearch(nums, midIndex + 1, right, target);
+                    return binarySearch(nums, incrementedMidIndex, right, target);
                 }
             }
         }
@@ -56,27 +58,27 @@ class Solution {
         if (shiftOnTheLeft) {
             // 1.1. target on the RIGHT
             if (doesValueOnTheRightIfRightIsNotShifted) {
-                return binarySearch(nums, midIndex + 1, right, target);
+                return binarySearch(nums, incrementedMidIndex, right, target);
             } else { // 1.2. target on the LEFT
-                return binarySearch(nums, left, midIndex - 1, target);
+                return binarySearch(nums, left, decrementedMidIndex, target);
             }
             // 2. else if: shift on the RIGHT
         } else if (shiftOnTheRight) {
             // 2.1. target on the LEFT
             if (doesValueOnTheLeftIfLeftIsNotShifted) {
-                return binarySearch(nums, left, midIndex - 1, target);
+                return binarySearch(nums, left, decrementedMidIndex, target);
             } else { // 2.2. target on the RIGHT
-                return binarySearch(nums, midIndex + 1, right, target);
+                return binarySearch(nums, incrementedMidIndex, right, target);
             }
             // 3. else: no shift in new arrays
         } else {
             // 3.1. if: value on the LEFT
             if (doesValueOnTheLeftIfLeftIsNotShifted) {
-                return binarySearch(nums, left, midIndex - 1, target);
+                return binarySearch(nums, left, decrementedMidIndex, target);
             }
             // 3.2. else: value on the RIGHT
             else {
-                return binarySearch(nums, midIndex + 1, right, target);
+                return binarySearch(nums, incrementedMidIndex, right, target);
             }
         }
     }
