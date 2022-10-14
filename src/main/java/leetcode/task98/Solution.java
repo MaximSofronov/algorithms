cb2c700f6936aed9b952811c79876dbc702cf9cd
Maksim Sofronov <Maksim_Sofronov@epam.com>
2022-10-14 12:31:55 +0600

    fixed task98

--- src/main/java/leetcode/task98/Solution.java
+++ src/main/java/leetcode/task98/Solution.java
@@ -1,24 +1,26 @@
 package leetcode.task98;
 
+import java.util.LinkedList;
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return validateChildren(root);
+        LinkedList<Integer> list = new LinkedList<>();
+
+        return validateBST(root, list);
     }
 
-    private boolean validateChildren(TreeNode node) {
+    private boolean validateBST(TreeNode node, LinkedList<Integer> list) {
         if (node == null) {
             return true;
         }
-        if (!validateChildren(node.left) || !validateChildren(node.right)) {
-            return false;
-        }
-        if (node.left != null && node.left.val >= node.val) {
-            return false;
-        }
-        if (node.right != null && node.right.val <= node.val) {
-            return false;
-        }
 
-        return true;
+        if (!validateBST(node.left, list)) {
+            return false;
+        }
+        if (!list.isEmpty() && list.getLast() >= node.val) {
+            return false;
+        }
+        list.add(node.val);
+        return validateBST(node.right, list);
     }
 }
